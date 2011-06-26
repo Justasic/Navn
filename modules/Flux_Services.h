03d2e92dd13b7af11ab015b59c177a978f18ad43
Justin Crawford <Justasic@Gmail.com>
2011-06-26 02:30:08 -0700

    Added a random message class that deathblade wanted to learn how it worked, this is a simple ported example from the mIRC bot HellHound-GuardDog

--- modules/Flux_Services.h
+++ modules/Flux_Services.h
@@ -4,8 +4,8 @@
 #include "../flux_net_irc.hpp"
 using namespace std;
 using namespace flux_net_irc;
+int idiots = 0; //define the idiot counter outside of the main loop
 void Flux_Services(Socket &sock, irc_string *reply){
-
 	if(reply->said("!part ")){ 
 		if(unick == owner_nick){
 			sock << privmsg(chan, "im out niggaz!");
@@ -26,7 +26,34 @@ void Flux_Services(Socket &sock, irc_string *reply){
 		}else{
 		sock << notice(unick, access_denied);
 		}
-	}/*
+	}
+	if(reply->said("slaps")){
+	 sock << privmsg(chan, "\0036Oh \0034Hell \0039No...");
+	 int num = randint(1,6); //make a random number from 1 to 6 (increase 6 to how ever many switch statements below) so messages are random
+	 switch(num){
+	   case 1:
+	     sock << me(chan, "\00313,1Dodges and \0037Pulls out a \0036Energy \0034Sword \0033and stabs\0032 %s \00312in the eye \0034.........what now \0039B1TCH?", unick.c_str());
+	     break; //break after each case so it doesnt keep trying to get the messages in the switch statement
+	   case 2:
+	     sock << me(chan, "\00313,1Dodges and \0037Shoots\0032 %s \00312in the \0034A\0038$\0039S \0033with a \0039AK47 \0034.......what now \0039B1TCH?", unick.c_str());
+	     break;
+	   case 3:
+	     sock << me(chan, "\00313,1Dodges and \0037Pulls out a \0036Ballistic Knife \0033and shoots\0032 %s \00312in the \00313DICK/PUSSY \0034.........what now \0039B1TCH?", unick.c_str());
+	     break;
+	   case 4:
+	     sock << me(chan, "\00313,1Dodges and \0037Pulls out a \0036Ray \0038Gun \0033and blows\0032 %s's \00312legs off \0034..........what now \0039B1TCH?", unick.c_str());
+	     break;
+	   case 5:
+	     sock << me(chan, "\00313,1Dodges and \0037Pulls out his \0034D1CK \0033and slaps\0032 %s \00312in the \0039Face \0034.........what now \0039B1TCH?", unick.c_str());
+	     break;
+	   case 6:
+	     sock << me(chan, "\0034,1bends dows and and \00312 grabs a M2W LAW and \0034 shoots back and \0038 SCORE it a win..................\0030 what now muthafucker!");
+	     break;
+	}
+	 idiots++; //add +1 to the already big number of idiots in the list
+	 sock << privmsg(chan, "\2\0034,1Total \0039morons \0033slapped \0038back \00310: %i", idiots); //tell the channel how many idiots where slapped.
+	}
+	/*
 	if(reply->said("!umode")){
 		if(unick == owner_nick){
 			string dest = reply->params(1);
