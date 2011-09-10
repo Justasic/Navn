f636ab5289efcbd5f6ff4566c20ac05175bfab63
Justin Crawford <Justasic@Gmail.com>
2011-09-09 18:10:22 -0700

    Moved Channel class to channel.h and channel.cpp, this makes things cleaner

--- user.h
+++ user.h
@@ -1,6 +1,7 @@
 #ifndef USER_H
 #define USER_H
 #include "privmsg.h"
+#include "channel.h"
 class User
 {
 public:
@@ -15,41 +16,6 @@ public:
   void SendMessage(const char*, ...);
   void SendPrivmsg(const Flux::string&);
   void SendPrivmsg(const char*, ...);
-};
-typedef Flux::map<Channel*> channel_map;
-class Channel
-{
-public:
-  Channel(const Flux::string&, time_t ts = time(NULL));
-  ~Channel();
-  //some day we will hhave a mode manager here :P
-  //some day we will also have a user finder for a channel ;P
-  Flux::string name;
-  Flux::string topic;
-  Flux::string topic_setter;
-  time_t topic_time;
-  time_t creation_time;
-  void SendJoin();
-  void SendPart();
-  void SendPart(const Flux::string&);
-  void SendPart(const char*, ...);
-  void SendWho();
-  void kick(User*, const Flux::string&);
-  void kick(User*, const char*, ...);
-  void kick(const Flux::string&, const Flux::string&);
-  void kick(const Flux::string&, const char*, ...);
-  void SetMode(const Flux::string&);
-  void SetMode(User *u, const Flux::string &mode);
-  void RemoveMode(const Flux::string&);
-  void RemoveMode(User *u, const Flux::string &mode);
-  void ChangeTopic(const Flux::string&);
-  void ChangeTopic(const char *fmt, ...);
-  void SendMessage(const Flux::string&);
-  void SendMessage(const char*, ...);
-  void SendAction(const Flux::string&);
-  void SendAction(const char*, ...);
-  void SendNotice(const Flux::string&);
-  void SendNotice(const char*, ...);
 };
 struct CommandSource
 {
