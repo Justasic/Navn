687eb9f66657389bfa65d1176081d21ff884623d
Justin Crawford <Justasic@Gmail.com>
2011-09-10 01:06:00 -0700

    Removed old include files from the repo

--- thread.h
+++
@@ -1,7 +0,0 @@
-/* All code is licensed under GNU General Public License GPL v3 (http://www.gnu.org/licenses/gpl.html) */
-#ifndef THREAD_H
-#define THREAD_H
-#include "includes.h"
-
-/*hehehe*/
-#endif
