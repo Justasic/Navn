cd8b275f30b435d297219b402f70226dab8df07a
Justin Crawford <Justasic@Gmail.com>
2011-09-28 21:27:56 -0700

    Completed the Thread engine, needs testing..

--- include/thread.h
+++ include/thread.h
@@ -1,15 +1,21 @@
 /* All code is licensed under GNU General Public License GPL v3 (http://www.gnu.org/licenses/gpl.html) */
 #ifndef THREAD_H
 #define THREAD_H
-#include <includes.h>
+#include "includes.h"
 
 class Thread
 {
+  pthread_t Handle;
+  bool exit;
 public:
+  /* Anope: "Exit the thread. Note that the thread still must be joined to free resources!" */
   Thread();
   virtual ~Thread();
+  void SetExitState();
+  void OnNotify();
+  void Notify();
+  bool GetExitState() const;
   void Join();
-  /* Anope: "Exit the thread. Note that the thread still must be joined to free resources!" */
   void Exit();
   void Start();
   virtual void ToRun() =0;
--- thread.cpp
+++ thread.cpp
@@ -1,5 +1,5 @@
 /* All code is licensed under GNU General Public License GPL v3 (http://www.gnu.org/licenses/gpl.html) */
-/*#include "thread.h"
+#include "thread.h"
 static inline pthread_attr_t *GetAttr()
 {
   static pthread_attr_t attr;
@@ -8,4 +8,44 @@ static inline pthread_attr_t *GetAttr()
   if(pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE))
     throw CoreException("Unable to make threads joinable");
   return &attr;
-}*/
+}
+Thread::Thread():exit(false) {}
+Thread::~Thread() {}
+void Thread::Notify() {}
+void *EntryPoint(void *parameter)
+{
+  Thread *thread = static_cast<Thread*>(parameter);
+  thread->ToRun();
+  thread->SetExitState();
+  pthread_exit(0);
+}
+void Thread::SetExitState()
+{
+ this->Notify();
+ exit = true;
+}
+bool Thread::GetExitState() const
+{
+ return exit; 
+}
+void Thread::OnNotify()
+{
+  this->Join();
+}
+void Thread::Start()
+{
+ if(pthread_create(&this->Handle, GetAttr(), EntryPoint, this))
+ {
+  throw CoreException("Could not Create Thread: "+Flux::stringify(strerror(errno)));
+ }
+}
+void Thread::Join()
+{
+  this->SetExitState();
+  pthread_join(Handle, NULL);
+}
+void Thread::Exit()
+{
+  this->SetExitState();
+  pthread_exit(0); 
+}
