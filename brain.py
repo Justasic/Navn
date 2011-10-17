e8c16fc8c6f4dfdf3a3b46a8426f9549f3df6218
lordofsraam <lordofsraam@ubuntu.(none)>
2011-10-16 20:09:48 -0700

    Encyclopedia module has been moved to C++.

--- brain.py
+++ brain.py
@@ -9,11 +9,11 @@ SN = "SEND_NOTHING"
 def remove_html_tags(data):
     p = re.compile(r'<.*?>')
     return p.sub('', data)
-    
+
 def remove_bracket_tags(data):
     p = re.compile(r'\[[^]]+]')
     return p.sub('', data)
-    
+
 def brain(raw_query = "c plus plus"):
 	query = urllib2.quote(raw_query)
 	r = requests.get('http://en.wikipedia.org/wiki/Special:Export?search='+query+'&go=Go')
@@ -22,13 +22,13 @@ def brain(raw_query = "c plus plus"):
 	for i in range(html_content.index("<p>"),html_content.index("</p>")):
 		first_paragraph += html_content[i]
 	return remove_bracket_tags(remove_html_tags(first_paragraph))
-	
+
 def stripFromEnd(victim,x):
 	toReturn = ""
 	for i in range(0,len(victim)-x):
 		toReturn += victim[i]
 	return toReturn
-  
+
 recieved = ""
 query = ""
 failedSearch = "For search options, see Help:Searching."
@@ -37,39 +37,10 @@ ambiguity = "may refer to:"
 for i in range(1,len(sys.argv)):
 	recieved += str(sys.argv[i]+" ")
 
-def handleInput(String,NumberOfWords):
-	params = String.split();
-	q = ""
-	for i in range(NumberOfWords,len(params)):
-		q += str(params[i]+" ")
-	query = q
-	return q
-	
-def handleBrain(Number):
-	information = brain(handleInput(recieved,Number))
-	testFS = stripFromEnd(information,1)
-	if ( testFS == failedSearch ):
-		print "Sorry, I couldn't find anything about that."
-	elif (information.find(ambiguity) != -1):
-		print "Please be a little more specific."
-	else:
-		print "'"+information+"'"
-
-if (recieved.find("Navn, what is a") != -1):
-	handleBrain(4)
-elif (recieved.find("Navn, what is") != -1):
-	handleBrain(3)
-elif(recieved.find("Navn, what are the") != -1):
-	handleBrain(4)		
-elif (recieved.find("Navn, what are") != -1):
-	handleBrain(3)
-elif (recieved.find("Navn, can you tell me about the") != -1):
-	handleBrain(7)
-elif (recieved.find("Navn, can you tell me about a") != -1):
-	handleBrain(7)
-elif (recieved.find("Navn, can you tell me about") != -1):
-	handleBrain(6)
-
-else:
-	print SN
-	
+query = urllib2.quote(recieved)
+r = requests.get('http://en.wikipedia.org/wiki/Special:Export?search='+query+'&go=Go')
+html_content = r.content
+first_paragraph = ""
+for i in range(html_content.index("<p>"),html_content.index("</p>")):
+	first_paragraph += html_content[i]
+print remove_bracket_tags(remove_html_tags(first_paragraph))
--- modules/encyclopedia.cpp
+++ modules/encyclopedia.cpp
@@ -5,7 +5,7 @@ class encyclopedia : public module
 
 public:
   encyclopedia(const Flux::string &Name):module(Name)
-  { 
+  {
     this->SetAuthor("Lordofsraam");
     this->SetVersion(VERSION);
     this->SetPriority(PRIORITY_LAST);
@@ -13,34 +13,42 @@ public:
   }
   void OnPrivmsg(User *u, Channel *c, const std::vector<Flux::string> &params)
   {
+    //std::vector<Flux::string> MessageParams = StringVector(params, ' ');
     Flux::string msg;
     for(unsigned i=0; i < params.size(); ++i)
       msg += params[i]+' ';
-      
-    Flux::string sn = "SEND_NOTHING";
 
     Flux::string cmd = params.empty()?"":params[0];
-    msg = msg.replace_all_cs("&","");
-    msg = msg.replace_all_cs("|","");
-    msg = msg.replace_all_cs(">","");
-    msg = msg.replace_all_cs("<","");
-    msg = msg.replace_all_cs("!","");
-    msg = msg.replace_all_cs("(","");
-    msg = msg.replace_all_cs(")","");
-    msg = msg.replace_all_cs("*","");
-    msg = msg.replace_all_cs("{","");
-    msg = msg.replace_all_cs("}","");
-    msg = msg.replace_all_cs("`","");
-    msg = msg.replace_all_cs("\"","");
-    msg = msg.replace_all_cs("\'","");
-    msg = msg.replace_all_cs(".","");
-    Flux::string str = "python brain.py "+msg;
-    
-    Flux::string information = execute(str.c_str());
-    information.trim();
-    if (information != sn)
+
+    if(cmd.equals_ci("!encyclopedia"))
     {
-      u->SendMessage(information);
+      Flux::string sn = "SEND_NOTHING";
+
+      Flux::string query;
+      for(unsigned i=1; i < params.size(); ++i) query += params[i]+' ';
+
+      query = query.replace_all_cs("&","");
+      query = query.replace_all_cs("|","");
+      query = query.replace_all_cs(">","");
+      query = query.replace_all_cs("<","");
+      query = query.replace_all_cs("!","");
+      query = query.replace_all_cs("(","");
+      query = query.replace_all_cs(")","");
+      query = query.replace_all_cs("*","");
+      query = query.replace_all_cs("{","");
+      query = query.replace_all_cs("}","");
+      query = query.replace_all_cs("`","");
+      query = query.replace_all_cs("\"","");
+      query = query.replace_all_cs("\'","");
+      query = query.replace_all_cs(".","");
+
+      Flux::string str = "python brain.py "+query;
+      Flux::string information = execute(str.c_str());
+      information.trim();
+      if (information != sn)
+      {
+	u->SendMessage(information);
+      }
     }
   }
 };
