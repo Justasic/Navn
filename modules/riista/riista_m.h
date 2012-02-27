ad9a824951b970277082b9bfb7dc9c144d816e90
Justin Crawford <Justasic@Gmail.com>
2012-02-27 15:06:28 -0800

    made most of riista work, now need to fix the way the source executes and shit

--- modules/riista/riista_m.h
+++ modules/riista/riista_m.h
@@ -468,9 +468,6 @@ public:
 
   void OnPrivmsgChannel(User *u, Channel *c, const std::vector<Flux::string> &params)
   {
-//     Flux::string command = params.empty()?"":params[0];
-//     if (command.empty())
-//       return;
     Flux::string message = CondenseString(params);
 
     riista->set_stats();
@@ -482,229 +479,209 @@ public:
     }
 
     //join_game command
-    if (message.search_ci("!join_game") && game_started) {
-      if (!in_player_list(u->nick)){
-	playersV.push_back(u->nick);
-	c->SendMessage("%s has joined the fight!", u->nick.c_str());
-	//job selection
-	if (message.search_ci("fighter")) {
-	  players[u->nick] = new Player(u->nick,"fighter");
-	  players[u->nick]->set_stats();
-	  c->SendMessage("%s is a Fighter.", u->nick.c_str());
+      if (message.search_ci("!join_game") && game_started) {
+	if (!in_player_list(u->nick)){
+	  playersV.push_back(u->nick);
+	  c->SendMessage("%s has joined the fight!", u->nick.c_str());
+	  //job selection
+	  if (message.search_ci("fighter")) {
+	    players[u->nick] = new Player(u->nick,"fighter");
+	    players[u->nick]->set_stats();
+	    c->SendMessage("%s is a Fighter.", u->nick.c_str());
+	  }
+	  else if (message.search_ci("mage")) {
+	    players[u->nick] = new Player(u->nick,"mage");
+	    players[u->nick]->set_stats();
+	    c->SendMessage("%s is a Mage.", u->nick.c_str());
+	  }
+	  else if (message.search_ci("rogue")) {
+	    players[u->nick] = new Player(u->nick,"rogue");
+	    players[u->nick]->set_stats();
+	    c->SendMessage("%s is a Rogue.", u->nick.c_str());
+	  }
+	  //if player didn't specify a valid job
+	  else {
+	    players[u->nick] = new Player(u->nick);
+	    players[u->nick]->set_stats();
+	    c->SendMessage("%s is clearly a n00b.", u->nick.c_str());
+	  }
+	  c->SendMessage(players[u->nick]->get_health());
+	  c->SendMessage("Current Players: ");
+	  c->SendMessage(print_vector(playersV));
+	  c->SendMessage("");
+	  //if more than one player has joined
+	  if (playersV.size() > 1) {
+	    c->SendMessage("%s is strengthened!", riista->name.c_str());
+	    riista->END += 5;
+	    riista->set_stats();
+	    c->ChangeTopic(riista->get_health());
+	  }
 	}
-	else if (message.search_ci("mage")) {
-	  players[u->nick] = new Player(u->nick,"mage");
-	  players[u->nick]->set_stats();
-	  c->SendMessage("%s is a Mage.", u->nick.c_str());
+	if (reply->said("!testing")){
+	  srand(time(NULL));
+	  int p1 = playersV.size();
+	  int p2 = rand()%p1;
+	  Flux::string victim = playersV[p2];
+	  cout << p1 << endl;
+	  cout << p2 << endl;
+	  cout << victim << endl;
 	}
-	else if (message.search_ci("rogue")) {
-	  players[u->nick] = new Player(u->nick,"rogue");
-	  players[u->nick]->set_stats();
-	  c->SendMessage("%s is a Rogue.", u->nick.c_str());
-	}
-	//if player didn't specify a valid job
-	else {
-	  players[u->nick] = new Player(u->nick);
-	  players[u->nick]->set_stats();
-	  c->SendMessage("%s is clearly a n00b.", u->nick.c_str());
-	}
-	c->SendMessage(players[u->nick]->get_health());
-	c->SendMessage("Current Players: ");
-	c->SendMessage(print_vector(playersV));
-	c->SendMessage("");
-	//if more than one player has joined
-	if (playersV.size() > 1) {
-	  c->SendMessage("%s is strengthened!", riista->name.c_str());
-	  riista->END += 5;
-	  riista->set_stats();
-	  c->ChangeTopic(riista->get_health());
-	}
-      }
-      //make sure someone who has died can't come back
-      else if (in_player_list(u->nick) && !players[u->nick]->alive()) {
-	c->SendMessage("You are dead...No zombies allowed...");
-      }
-      //in case someone tries to join twice under the same nick
-      else {
-	c->SendMessage("You are already in the game!");
-      }
-    } else if (message.search_ci("!join_game") && !game_started)
-    {
-      c->SendMessage("Please start the game before joining.");
-  }
-  void riista_m(SendMessage *Send, irc_string *reply){
-    
-    //Game code here:
-    
-    //start_game command
-//     if (reply->said(start_game)){
-//       Send->privmsg(channel, riista->name+" has arrived! Let the battle begin!");
-//       Send->command->topic(channel,riista->get_health());
-//       game_started = true;
-//     }
-    
 
-    
-    //if somone tries to join before the game is started
-    if (reply->said("!join_game") && !game_started) {
-      Send->privmsg(channel, "Please start the game before joining.");
-    }
-    
-    if (reply->said("!testing")){
-      srand(time(NULL));
-      int p1 = playersV.size();
-      int p2 = rand()%p1;
-      Flux::string victim = playersV[p2];
-      cout << p1 << endl;
-      cout << p2 << endl;
-      cout << victim << endl;
-    }
-    
-    //attack command
-    if (reply->said("!attack") && game_started){
-      if (in_player_list(unick) && players[unick]->alive()) {
-	if (reply->said("slash")){
-	  Send->privmsg(channel, riista->hurt(unick, "slash"));
-	  sock << topic(channel,riista->get_health());
-	}
-	else if (reply->said("cross_cut")){
-	  Send->privmsg(channel, riista->hurt(unick, "cross_cut"));
-	  sock << topic(channel,riista->get_health());
-	}
-	else if (reply->said("lucky_shot")){
-	  Send->privmsg(channel, riista->hurt(unick, "lucky_shot"));
-	  sock << topic(channel,riista->get_health());
-	}
-	//if player didn't specify a valid action
-	else {
-	  Send->privmsg(channel, "Wait, what? ");
-	}
-	//riista's turn
-	if(riista->last_attacker == riista->last_attacker2 && riista->last_attacker2 == riista->last_attacker3){
-	  Send->privmsg(channel, riista->retaliate(riista->last_attacker));
-	}else{Send->privmsg(channel, riista->retaliate());}
-      } else {
-	Send->privmsg(channel, "You are not in the game");
-      }
-    }
-    
-    //magic command
-    if (reply->said("!magic") && game_started){
-      if (in_player_list(unick) && players[unick]->alive()) {
-	if (reply->said("heal")){
-	  bool already_cast = false;
-	  for(unsigned i = 0; i < playersV.size(); i++){
-	    if (reply->said(playersV[i]) && playersV[i] != unick && players[playersV[i]]->alive()){
-	      if (players[unick]->mp >= 8) {
-		stringstream output;
-		int heals = (.05*players[playersV[i]]->INT)*players[playersV[i]]->maxHp;
-		players[playersV[i]]->hp += heals;
-		players[unick]->mp -= 8;
-		if (players[playersV[i]]->hp > players[playersV[i]]->maxHp) players[playersV[i]]->hp = players[playersV[i]]->maxHp;
-			output << players[unick]->name << " casts Heal on " << playersV[i] << " and heals them by " << heals << " HP!";
-		Send->privmsg(channel, output.str());
-	      }else{
-		Send->privmsg(channel, "Not enough MP or target is dead.");
-	      }
-	      already_cast = true;
-	      break;
+	if (message.search_ci("!attack") && game_started){
+	  if (in_player_list(u->nick) && players[u->nick]->alive()) {
+	    if (message.search_ci("slash")){
+	      c->SendMessage(riista->hurt(u->nick, "slash"));
+	      c->ChangeTopic(riista->get_health());
 	    }
-	  }
-	  if(!already_cast){
-	    Send->privmsg(channel, players[unick]->heal());
-	    Send->privmsg(channel, players[unick]->get_health());
+	    else if (message.search_ci("cross_cut")){
+	      c->SendMessage(riista->hurt(u->nick, "cross_cut"));
+	      c->ChangeTopic(riista->get_health());
+	    }
+	    else if (message.search_ci("lucky_shot")){
+	      c->SendMessage(riista->hurt(u->nick, "lucky_shot"));
+	      c->ChangeTopic(riista->get_health());
+	    }
+	    //if player didn't specify a valid action
+	    else {
+	      c->SendMessage("Wait, what? ");
+	    }
+	    //riista's turn
+	    if(riista->last_attacker == riista->last_attacker2 && riista->last_attacker2 == riista->last_attacker3){
+	      c->SendMessage(riista->retaliate(riista->last_attacker));
+	    }else{c->SendMessage(riista->retaliate());}
+	  } else {
+	    c->SendMessage("You are not in the game");
 	  }
 	}
-	else if(reply->said("paralyze")){
-	  if (players[unick]->job == "Mage" or players[unick]->job == "Rogue"){
-	    if (players[unick]->mp > 20){
-	      if(!riista->paralyzed){
-		Send->privmsg(channel, unick+" is attempting to paralyze "+riista->name);
-		players[unick]->mp -= 20;
-		srand(time(NULL));
-		int ri = rand()%100;
-		if (ri >= (100-((players[unick]->INT * 8)-(riista->AGI * 2)))){
-		  riista->paralyzed = true;
-		  Send->privmsg(channel, "The paralysis succeeded! "+riista->name+" is paralyzed!");
-		}else{Send->privmsg(channel, "The paralysis failed!");}
+
+	//make sure someone who has died can't come back
+	else if (in_player_list(u->nick) && !players[u->nick]->alive()) {
+	  c->SendMessage("You are dead...No zombies allowed...");
+	}
+	//in case someone tries to join twice under the same nick
+	else {
+	  c->SendMessage("You are already in the game!");
+	}
+      } else if (message.search_ci("!join_game") && !game_started)
+      {
+	c->SendMessage("Please start the game before joining.");
+
+	if (message.search_ci("!magic") && game_started){
+	  if (in_player_list(u->nick) && players[u->nick]->alive()) {
+	    if (message.search_ci("heal")){
+	      bool already_cast = false;
+	      for(unsigned i = 0; i < playersV.size(); i++){
+		if (message.search_ci(playersV[i]) && playersV[i] != unick && players[playersV[i]]->alive()){
+		  if (players[unick]->mp >= 8) {
+		    std::stringstream output;
+		    int heals = (.05*players[playersV[i]]->INT)*players[playersV[i]]->maxHp;
+		    players[playersV[i]]->hp += heals;
+		    players[u->nick]->mp -= 8;
+		    if (players[playersV[i]]->hp > players[playersV[i]]->maxHp) players[playersV[i]]->hp = players[playersV[i]]->maxHp;
+		    output << players[u->nick]->name << " casts Heal on " << playersV[i] << " and heals them by " << heals << " HP!";
+		    c->SendMessage(output.str());
+		  }else{
+		    c->SendMessage("Not enough MP or target is dead.");
+		  }
+		  already_cast = true;
+		  break;
+		}
 	      }
-	    }else{Send->privmsg(channel, "Not enough MP!");}
-	}elseSend->privmsg(channel, "You need to be a mage or a rogue to use this spell "+unick);}
+	      if(!already_cast){
+		c->SendMessage(players[u->nick]->heal());
+		c->SendMessage(players[u->nick]->get_health());
+	      }
+	    }
+	    else if(reply->said("paralyze")){
+	      if (players[u->nick]->job == "Mage" or players[u->nick]->job == "Rogue"){
+		if (players[u->nick]->mp > 20){
+		  if(!riista->paralyzed){
+		    c->SendMessage("%s is attempting to paralyze %s", u->nick.c_str(), riista->name.c_str());
+		    players[u->nick]->mp -= 20;
+		    srand(time(NULL));
+		    int ri = rand()%100;
+		    if (ri >= (100-((players[u->nick]->INT * 8)-(riista->AGI * 2)))){
+		      riista->paralyzed = true;
+		      c->SendMessage("The paralysis succeeded! %s is paralyzed!", riista->name.c_str());
+		    }else{c->SendMessage("The paralysis failed!");}
+		  }
+		}else{c->SendMessage("Not enough MP!");}
+	      }else
+		c->SendMessage("You need to be a mage or a rogue to use this spell %s", u->nick.c_str());}
+	  }
+	  else if(message.search_ci("concentrate")){
+	    if (players[u->nick]->job == "Mage"){
+	      c->SendMessage(players[u->nick]->concentrate(u->nick));
+	      if(players[u->nick]->mp > players[u->nick]->maxMp) players[u->nick]->mp = players[u->nick]->maxMp;
+	      c->SendMessage(riista->retaliate(u->nick));
+	    }else{c->SendMessage("You need to be a mage or a rogue to use this spell %s ", u->nick.c_str());}
+      }
+      else if(message.search_ci("forceus")){
+	if (players[u->nick]->job == "Mage"){
+	  c->SendMessage(riista->hurt(u->nick, "forceus"));
+	  c->ChangeTopic(riista->get_health());
+	}else{c->SendMessage("You need to be a mage or a rogue to use this spell %s", u->nick.c_str());}
+      }
+      //if player didn't specify a valid action
+      else {
+	c->SendMessage("Wait, what? ");
+      }
+      //riista's turn
+      if(riista->last_attacker == riista->last_attacker2 && riista->last_attacker2 == riista->last_attacker3){
+	c->SendMessage(riista->retaliate(riista->last_attacker));
+      }else{
+	c->SendMessage(riista->retaliate());
+	c->SendMessage(players[u->nick]->get_health());
+      }
+    } else {
+      c->SendMessage("You are not in the game");
     }
-    else if(reply->said("concentrate")){
-	      if (players[unick]->job == "Mage"){
-		Send->privmsg(channel, players[unick]->concentrate(unick));
-		if(players[unick]->mp > players[unick]->maxMp) players[unick]->mp = players[unick]->maxMp;
-				    Send->privmsg(channel, riista->retaliate(unick));
-	      }else{Send->privmsg(channel, "You need to be a mage or a rogue to use this spell "+unick);}
-    }
-    else if(reply->said("forceus")){
-		  if (players[unick]->job == "Mage"){
-		    Send->privmsg(channel, riista->hurt(unick, "forceus"));
-		    sock << topic(channel,riista->get_health());
-		  }else{Send->privmsg(channel, "You need to be a mage or a rogue to use this spell "+unick);}
-    }
-    //if player didn't specify a valid action
-    else {
-      Send->privmsg(channel, "Wait, what? ");
-    }
-    //riista's turn
-    if(riista->last_attacker == riista->last_attacker2 && riista->last_attacker2 == riista->last_attacker3){
-      Send->privmsg(channel, riista->retaliate(riista->last_attacker));
-    }else{
-      Send->privmsg(channel, riista->retaliate());
-      Send->privmsg(channel, players[unick]->get_health());
-    }
-  } else {
-    Send->privmsg(channel, "You are not in the game");
-  }
-}//magic commands end here
+    if (message.search_ci("!stance") && game_started){
+      if (in_player_list(u->nick) && players[u->nick]->alive()) {
+	if (message.search_ci("defending")){
+	  c->SendMessage(players[u->nick]->set_stance("defending"));
+      }
+      else if(message.search_ci("aggressive")){
+	c->SendMessage(players[u->nick]->set_stance("aggressive"));
+      }
+      else if(message.search_ci("running")){
+	c->SendMessage(players[u->nick]->set_stance("running"));
+      }
+      else if(message.search_ci("normal")){
+	c->SendMessage(players[u->nick]->set_stance("normal"));
+      }
+      }else{c->SendMessage("You are dead.");}
+      }
+      
+      if (reply->said("!my_stats")){
+	  if (in_player_list(u->nick)){
+	    u->SendMessage(players[u->nick]->get_health());
+	  }else{
+	    u->SendMessage("You are not in the game");
+	  }
+      }
+      
+      
+      if (game_started && riista->alive() && playersV.size() > 0){
+	int ri = randint(1,100);
+	if (ri <= 10){
+	  c->SendMessage("Has suddenly become enraged!");
+	  c->SendMessage(riista->retaliate());
+	}
+      }
 
-if (reply->said("!stance") && game_started){
-  if (in_player_list(unick) && players[unick]->alive()) {
-    if (reply->said("defending") or reply->said("Defending")){
-		      Send->privmsg(channel, players[unick]->set_stance("defending"));
-    }
-    else if(reply->said("aggressive") or reply->said("Aggressive")){
-	    Send->privmsg(channel, players[unick]->set_stance("aggressive"));
-    }
-    else if(reply->said("running") or reply->said("Running")){
-	  Send->privmsg(channel, players[unick]->set_stance("running"));
-    }
-    else if(reply->said("normal") or reply->said("Normal")){
-	    Send->privmsg(channel, players[unick]->set_stance("normal"));
-    }
-  }else{Send->privmsg(channel, "You are dead.");}
-}
 
-if (reply->said("!my_stats")){
-  if (in_player_list(unick)){
-    sock << notice(unick, players[unick]->get_health());
-  }else{
-    sock << notice(unick, "You are not in the game");
-  }
-  }
-  
-  
-  if (game_started && riista->alive() && playersV.size() > 0){
-    int ri = randint(1,100);
-    if (ri <= 10){
-      Send->privmsg(channel, "Has suddenly become enraged!");
-      Send->privmsg(channel, riista->retaliate());
-    }
-  }
-  
-  
-  if (!riista->alive()){
-    /*continue;
-  }else{*/
-    Send->privmsg(channel, riista->name+" is dead!");
-    Send->privmsg(channel, riista->last_attacker+" killed "+riista->name+"!");
-    sock << quit("SCREW you guys! I'M going Home!");
-    delete riista;
-    exit(EXIT_FAILURE);
-  }
-  
+      if (!riista->alive()){
+	/*continue;
+      }else{*/
+	c->SendMessage(riista->name+" is dead!");
+	c->SendMessage(riista->last_attacker+" killed "+riista->name+"!");
+	sock << quit("SCREW you guys! I'M going Home!");
+	delete riista;
+	exit(EXIT_FAILURE);
+      }
+  }//magic commands end here
+
   }
 };
 
