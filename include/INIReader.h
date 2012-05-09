2f1ced2a353462cc665672f6bcc2777c48e09e69
Justin Crawford <Justasic@Gmail.com>
2012-05-08 19:26:19 -0700

    Updated ChangeLog and fixed some compile errors

--- include/INIReader.h
+++ include/INIReader.h
@@ -85,6 +85,7 @@ public:
   Flux::string AutoIdentString;
   Flux::string ServicesService;
   Flux::string WelcomeMessage;
+  Flux::string LogColor;
   Flux::string luadir;
   Flux::vector Owners;
   int LogAge;
